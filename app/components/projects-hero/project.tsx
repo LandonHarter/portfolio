088f290e62f0d70f3b9b23a9f64b4c5637acfbdc
LandonHarter <landonharter@outlook.com>
2023-02-05 21:07:28 -0500

    button icons

--- app/components/projects-hero/project.tsx
+++ app/components/projects-hero/project.tsx
@@ -12,8 +12,8 @@ export default function ProjectHeroExample(props:any) {
                 <p>{props.desc}</p>
 
                 <div className="project-button-container">
-                    <Link href={props.link} target="_blank"><button>Visit Project</button></Link>
-                    <Link href={props.code} target="_blank"><button>Source Code</button></Link>
+                    <Link href={props.link} target="_blank" style={{textDecoration: "none"}}><button><img src="/home.png" className="img-button" />Visit Project</button></Link>
+                    <Link href={props.code} target="_blank" style={{textDecoration: "none"}}><button><img src="/skills/github.png" className="img-button" />Source Code</button></Link>
                 </div>
             </div>
         </div>
