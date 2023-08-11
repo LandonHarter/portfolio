24a22683d276bd37aa0ceb58586bd9a9ee273d02
LandonHarter <landonharter@outlook.com>
2023-08-10 21:51:32 -0400

    implement page transition

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -6,7 +6,7 @@ import { useContext } from 'react';
 import ThemeContext from '@context/theme';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
-import PageTransitionContext from '@context/transition';
+import PageTransitionContext, { handlePageTransition } from '@context/transition';
 
 export default function Header() {
     const router = useRouter();
@@ -17,7 +17,9 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <section className={styles.header_left}>
-                <Link href='/'>
+                <Link href='/' onClick={(e) => {
+                    handlePageTransition(e, '/', router, pushPageTransition);
+                }}>
                     <Image src={`/images/logos/logo-${theme === 'dark' ? 'light' : 'dark'}.png`} alt="Landon Harter Logo" width={55} height={55} className={styles.logo} />
                 </Link>
             </section>
@@ -26,17 +28,15 @@ export default function Header() {
                     <ul className={styles.nav_ul}>
                         <li className={`${styles.nav_link} ${route === '' && styles.nav_link_active}`}>
                             <Link href='/' onClick={(e) => {
-                                e.preventDefault();
-                                pushPageTransition();
-                                setTimeout(() => {
-                                    router.push('/');
-                                }, 750);
+                                handlePageTransition(e, '/', router, pushPageTransition);
                             }}>
                                 Home
                             </Link>
                         </li>
                         <li className={`${styles.nav_link} ${route === 'projects' && styles.nav_link_active}`}>
-                            <Link href='/projects'>
+                            <Link href='/projects' onClick={(e) => {
+                                handlePageTransition(e, '/projects', router, pushPageTransition);
+                            }}>
                                 Projects
                             </Link>
                         </li>
--- app/context/transition.ts
+++ app/context/transition.ts
@@ -1,4 +1,13 @@
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import { createContext } from "react";
 
 const PageTransitionContext = createContext<Function>(() => { });
 export default PageTransitionContext;
+
+export function handlePageTransition(event: any, route: string, router: AppRouterInstance, pushPageTransition: Function) {
+    event.preventDefault();
+    pushPageTransition();
+    setTimeout(() => {
+        router.push(route);
+    }, 750);
+}
