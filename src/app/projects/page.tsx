4629b1444935d08cb5d3fec4742aaa3f2bc8cb6f
LandonHarter <landonharter@outlook.com>
2025-05-15 18:04:15 -0400

    update projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,6 +15,13 @@ export default function Projects() {
 				time="2025 - Present"
 			/>
 			<Project
+				name="dev.kodisc"
+				description="The incredible technology behind Kodisc, all behind an API."
+				href="https://dev.kodisc.com"
+				role="Founder"
+				time="2025 - Present"
+			/>
+			<Project
 				name="Draftl"
 				description="Fixing college essays with AI one line at a time."
 				href="https://draftl.com"
@@ -29,18 +36,25 @@ export default function Projects() {
 				time="2024 - Present"
 			/>
 			<Project
+				name="Hackathon"
+				description="Projects built in 2 or 3 days for hackathons."
+				href="https://devpost.com/LandonHarter"
+				role="Developer"
+				time="2024 - Present"
+			/>
+			<Project
 				name="Opal"
 				description="A new way to study, powered by AI."
 				href="https://learnopal.com"
 				role="CoFounder"
-				time="2024 - Present"
+				time="2024 - 2025"
 			/>
 			<Project
 				name="Radium"
 				description="A full game engine built in Java. Wow."
 				href="https://github.com/radiumgame/radium"
 				role="Founder"
-				time="2021 - Present"
+				time="2021 - 2023"
 			/>
 		</main>
 	);
